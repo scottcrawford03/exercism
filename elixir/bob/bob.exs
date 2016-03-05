8857471c53dd3c4d36ee3cf98f64b202883e05f9
Scott Crawford <scottcrawford03@gmail.com>
2016-03-04 21:08:11 -0500

    remove A-Z and add ends_with/2 for bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,10 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+      String.strip(input) == "" -> "Fine. Be that way!"
+      String.ends_with?(input, "?") -> "Sure."
+      String.match?(input, ~r/[\p{L}]/) && String.upcase(input) == input -> "Whoa, chill out!"
+      true -> "Whatever."
     end
   end
 end
